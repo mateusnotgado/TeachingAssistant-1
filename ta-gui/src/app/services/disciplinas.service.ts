2d2bfd80cb28d472dca700dad8e9f4728b894fe7
Matheus Aquatico <mmml2@cin.ufpe.br>
2021-12-17 17:48:41 -0300

    Mudanças em disciplinas.service

--- ta-gui/src/app/services/disciplinas.service.ts
+++ ta-gui/src/app/services/disciplinas.service.ts
@@ -12,8 +12,9 @@ export class DisciplinasService {
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   private taURL = 'http://localhost:3000';
 
+  disciplinas: Disciplina[]
   constructor(private http: HttpClient) { 
-
+    this.disciplinas=[];
   }
   criar(disciplina: Disciplina): Observable<Disciplina> {
     return this.http.post<any>(this.taURL + "/disciplina", disciplina, { headers: this.headers })
