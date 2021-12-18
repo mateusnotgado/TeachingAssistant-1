18d1036e45c654686ad24b8328a2a9aeb0301a3a
Matheus Aquatico <mmml2@cin.ufpe.br>
2021-12-18 12:20:26 -0300

    Adicionado componentes de oferta e cadastro de disciplinas na pagina principal

--- ta-gui/src/app/app-routing.module.ts
+++ ta-gui/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaDisciplinasComponent } from './pagina-disciplinas/pagina-disciplinas.component';
+import { PaginaOfertaDisciplinasComponent } from './pagina-oferta-disciplinas/pagina-oferta-disciplinas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: PaginaDisciplinasComponent,
+    path:  'CadastroDisciplinas'
+  },
+
+  {
+    component: PaginaOfertaDisciplinasComponent,
+    path:  'OfertaDisciplinas'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
